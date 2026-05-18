f48e543c7e5f375fd8907cc25eea1fa98f0e003e
PRAKASHOMVAR <omvaradhan@gmail.com>
2026-05-18 11:43:01 +0530

    Frontend Ready For Deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"http://localhost:5000/api"
+  baseURL: "https://ecmmerce-backend-qs4l.onrender.com/api"
 });
 
 API.interceptors.request.use((req)=>{
